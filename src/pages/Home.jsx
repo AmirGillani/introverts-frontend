e4b4a6e319f3c48199d24f573d574e26fdcb4ba9
amir <amirgillani160@gmail.com>
2025-04-14 20:00:59 +0500

    Almost final Changes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,30 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 import Posts from "../components/Posts";
 
+import { io } from "socket.io-client";
+import { useSelector } from "react-redux";
 
 export default function Home() {
+  const { user } = useSelector((state) => state.auth);
+
+  const socket = io("https://introverts-backend.vercel.app");
+
+  useEffect(() => {
+    // Register user when component mounts
+    socket.emit("register", user._id);
+
+    // Listen for notifications
+    socket.on("notification", (msg) => {
+      alert(`🔔 Notification: ${msg}`);
+    });
+
+    // Cleanup on unmount
+    return () => {
+      socket.off("notification");
+    };
+  }, [user._id, socket]);
+
   return (
     <div>
       <Posts />
